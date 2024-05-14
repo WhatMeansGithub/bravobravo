7bf6c74c0c7119ecc20da6a82e2030943d52fc1e
RogerIsk <rogeriskrenov@gmail.com>
2024-05-14 11:26:23 +0200

    A small programm using while and for loops. Commit 1

---
+++ main.py
@@ -0,0 +1,7 @@
+
+list = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+
+while True:         
+    for i in list: 
+        print(i)
+    break           # without this line, the loop won't stop running
