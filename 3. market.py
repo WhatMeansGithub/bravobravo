0fd59a2a198c3693f738eca620786b16cc4fe47f
RogerIsk <rogeriskrenov@gmail.com>
2024-05-22 12:43:42 +0200

    Commit 7. Button that looks up and shows all the Major Indices shares/stocks

--- 3. market.py
+++ 3. market.py
@@ -28,21 +28,22 @@ def process_table(table):                                           # Function t
         df = pd.DataFrame(rows, columns=headers)                    # Creating a DataFrame from the headers and rows
         df.index = df.index + 1                                     # Incrementing the index by 1 so it starts from 1 not 0
         print(df)                                                   # Displaying the DataFrame (table basically)
+        output_string.set(df)                                       # Displaying a message in the GUI
     else:
         print("No table found with the specified class.")           # If the table does not exist, print this message (it was useful to check for errors)
 
 def get_major_indices():                                              # Function to get the stock market for major indices from the web
-    url1 = 'https://www.investing.com/indices/major-indices'    # URL of the website we will scrape
-    page1 = requests.get(url1)                                  # Requesting the HTML content of the website
-    soup1 = BeautifulSoup(page1.text, 'html.parser')            # Parsing the HTML content # Extracting text from each table and stripping whitespace
-    all_variables1 = soup1.find_all('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
-    all_variables1 = [table.text.strip() for table in all_variables1] # Extracting text from each table and stripping whitespace
+    url = 'https://www.investing.com/indices/major-indices'    # URL of the website we will scrape
+    page = requests.get(url)                                  # Requesting the HTML content of the website
+    soup = BeautifulSoup(page.text, 'html.parser')            # Parsing the HTML content # Extracting text from each table and stripping whitespace
+    all_variables = soup.find_all('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
+    all_variables = [table.text.strip() for table in all_variables] # Extracting text from each table and stripping whitespace
 
-    special_table = soup1.find('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
+    special_table = soup.find('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
     process_table(special_table)
 
     data = []                                           # we create a list to store data from the table
-    for table in all_variables1:                        # Looping through each table
+    for table in all_variables:                        # Looping through each table
         lines = table.split('\n')                       # Splitting the table into lines
         headers = lines[0].split()                      # Assuming the first line contains headers
         rows = [line.split() for line in lines[1:]]     # Assuming the rest of the lines contain rows
@@ -51,19 +52,19 @@ def get_major_indices():                                              # Function
         df = create_dataframe(headers, rows)            # Creating a DataFrame from the headers and rows
         filename = f'market_table_{i+1}.csv'            # Create a filename for each table
         df.to_csv(filename, index=False)                # Save the DataFrame to a CSV file
-
+    
 def get_trending_stocks():                                      # Function to get the stocks that are currently trending
-    url1 = 'https://www.investing.com/equities/trending-stocks'    # URL of the website we will scrape
-    page1 = requests.get(url1)                                  # Requesting the HTML content of the website
-    soup1 = BeautifulSoup(page1.text, 'html.parser')            # Parsing the HTML content # Extracting text from each table and stripping whitespace
-    all_variables1 = soup1.find_all('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
-    all_variables1 = [table.text.strip() for table in all_variables1] # Extracting text from each table and stripping whitespace
+    url = 'https://www.investing.com/equities/trending-stocks'    # URL of the website we will scrape
+    page = requests.get(url)                                  # Requesting the HTML content of the website
+    soup = BeautifulSoup(page.text, 'html.parser')            # Parsing the HTML content # Extracting text from each table and stripping whitespace
+    all_variables = soup.find_all('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
+    all_variables = [table.text.strip() for table in all_variables] # Extracting text from each table and stripping whitespace
 
-    special_table = soup1.find('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
+    special_table = soup.find('table' , class_="datatable-v2_table__93S4Y dynamic-table-v2_dynamic-table__iz42m datatable-v2_table--mobile-basic__uC0U0 datatable-v2_table--freeze-column__uGXoD undefined")
     process_table(special_table)
 
     data = []                                           # we create a list to store data from the table
-    for table in all_variables1:                        # Looping through each table
+    for table in all_variables:                        # Looping through each table
         lines = table.split('\n')                       # Splitting the table into lines
         headers = lines[0].split()                      # Assuming the first line contains headers
         rows = [line.split() for line in lines[1:]]     # Assuming the rest of the lines contain rows
@@ -78,7 +79,7 @@ def get_trending_stocks():                                      # Function to ge
 # window
 window = tk.Tk()                # Creating a tkinter window
 window.title("Market Shares")   # Setting the title of the window
-window.geometry("800x600")      # Setting the size of the window
+window.geometry("1200x1000")      # Setting the size of the window
 
 # title
 title = ttk.Label(master = window, text="Major Indices", font=("Calibri 30 bold"))   # Creating a label widget
@@ -87,11 +88,17 @@ title.pack()                                                                # Di
 # input field
 input_frame = ttk.Frame(master = window)                    # Creating a frame widget
 entry = ttk.Entry(master = input_frame)                     # Creating an entry widget
-button = ttk.Button(master = input_frame, text="Search")    # Creating a button widget
+button = ttk.Button(master = input_frame, text="Get Indices", command = get_major_indices)    # Creating a button widget
+entry_int = ttk.Entry(master = input_frame)                 # Creating an entry widget
 entry.pack(side = 'left', padx = 10)                                  # Displaying the entry widget
 button.pack(side = 'left')                                # Displaying the button widget
 input_frame.pack(pady = 20)                                          # Displaying the frame widget
 
+# output field
+output_string = tk.StringVar()                                                                  # Creating a string variable
+output_lable = ttk.Label(master = window, font = 'Calibri 15' , textvariable = output_string)   # Creating a label widget
+output_lable.pack()                                                                             # Displaying the label widget
+output_string.set("Data has been saved to CSV files.")                                          # Displaying a message in the GUI
 
 # run
 window.mainloop()               # Running the tkinter window
